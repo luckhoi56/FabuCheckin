9a11b5134e686699e21109847d4c2c0f694641a4
Eric Luc <luckhoi89@gmail.com>
2021-10-07 22:06:49 -0700

    ok kind of work

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -16,6 +16,7 @@
   let m_chosenDate = new Date();
   let m_message_error = "";
   let m_message_error_modal = false;
+  let m_customer_update = false
   let m_customer_appointment = {
     firstName: "",
     lastName: "",
@@ -507,17 +508,17 @@
               class="text-lg leading-6 font-medium text-gray-900"
               id="modal-title"
             >
-              {customer.firstName}
+              {customer.firstName} {customer.lastName}
             </h3>
-            <div class="mt-2">
-              <p class="text-sm text-gray-500">
-                A confirmation message will be sent
-              </p>
-            </div>
+            
           </div>
         </div>
         <div class="float-right justify-center mt-6 grid grid-cols-2 gap-5">
-          <button type="button" class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+          <button 
+          on:click={() => {
+            m_customer_update = true;
+          }}
+          type="button" class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
             Update
           </button>
 
@@ -630,6 +631,187 @@
   </div>
 {/if}
 
+
+
+{#if m_customer_update === true}
+  <!-- Modal -->
+  <div
+    transition:fade
+    class="fixed z-10 inset-0 overflow-y-auto"
+    aria-labelledby="modal-title"
+    role="dialog"
+    aria-modal="true"
+  >
+    <div
+      class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0"
+    >
+      <!--
+      Background overlay, show/hide based on modal state.
+
+      Entering: "ease-out duration-300"
+        From: "opacity-0"
+        To: "opacity-100"
+      Leaving: "ease-in duration-200"
+        From: "opacity-100"
+        To: "opacity-0"
+    -->
+      <div
+        class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
+        aria-hidden="true"
+      />
+
+      <!-- This element is to trick the browser into centering the modal contents. -->
+      <span
+        class="hidden sm:inline-block sm:align-middle sm:h-screen"
+        aria-hidden="true">&#8203;</span
+      >
+
+      <!--
+      Modal panel, show/hide based on modal state.
+
+      Entering: "ease-out duration-300"
+        From: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+        To: "opacity-100 translate-y-0 sm:scale-100"
+      Leaving: "ease-in duration-200"
+        From: "opacity-100 translate-y-0 sm:scale-100"
+        To: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+    -->
+      <div
+        class="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6"
+      >
+        <!--
+
+       <!--
+  This example requires Tailwind CSS v2.0+ 
+  
+  This example requires some changes to your config:
+  
+  ```
+  // tailwind.config.js
+  module.exports = {
+    // ...
+    plugins: [
+      // ...
+      require('@tailwindcss/forms'),
+    ]
+  }
+  ```
+-->
+        <form class="space-y-8 divide-y divide-gray-200">
+          <div class="space-y-8 divide-y divide-gray-200 sm:space-y-5">
+            <div class="pt-8 space-y-6 sm:pt-10 sm:space-y-5">
+              <div>
+                <h3 class="text-lg leading-6 font-medium text-gray-900">
+                  Checkin Information Update
+                </h3>
+                <br>
+              
+             
+
+
+                <div class="space-y-6 sm:space-y-5">
+                  <div
+                    class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5"
+                  >
+                    <label
+                      for="first_name"
+                      class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
+                    >
+                      First name
+                    </label>
+                    <div class="mt-1 sm:mt-0 sm:col-span-2">
+                      <input
+                        bind:value={m_customer_appointment["firstName"]}
+                        type="text"
+                        name="first_name"
+                        id="first_name"
+                        autocomplete="given-name"
+                        class="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md"
+                        required
+                      />
+                    </div>
+                  </div>
+  
+                  <div
+                    class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5"
+                  >
+                    <label
+                      for="last_name"
+                      class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
+                    >
+                      Last name
+                    </label>
+                    <div class="mt-1 sm:mt-0 sm:col-span-2">
+                      <input
+                        bind:value={m_customer_appointment["lastName"]}
+                        type="text"
+                        name="last_name"
+                        id="last_name"
+                        autocomplete="family-name"
+                        class="max-w-lg block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md"
+                        required
+                      />
+                    </div>
+                  </div>
+  
+                
+
+               
+
+               
+
+
+        
+
+                
+              </div>
+            </div>
+          </div>
+        </form>
+
+        <div
+          class="mt-5 sm:mt-6 sm:grid sm:grid-cols-2 sm:gap-3 sm:grid-flow-row-dense"
+        > 
+          {#await getServer()}
+            <button
+            
+            type="button"
+            class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-yellow-700 text-base font-medium text-white hover:bg-yellow-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500 sm:col-start-2 sm:text-sm"
+          >
+            Waiting
+          </button>
+          {:then data}
+             <button
+            on:click={sendCheckin}
+            type="button"
+            class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-yellow-700 text-base font-medium text-white hover:bg-yellow-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500 sm:col-start-2 sm:text-sm"
+          >
+            Confirm
+          </button>
+          {:catch error}
+             <button
+           
+            type="button"
+            class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-yellow-700 text-base font-medium text-white hover:bg-yellow-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500 sm:col-start-2 sm:text-sm"
+          >
+            Error: Cannot GET
+          </button>
+          {/await}
+         
+          <button
+            on:click={() => {
+              m_modal = m_customer_update = m_customer_found =false;
+            }}
+            type="button"
+            class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500 sm:mt-0 sm:col-start-1 sm:text-sm"
+          >
+            Cancel Check In Information Update
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+{/if}
 <style>
   .required:after {
     content:" *";
