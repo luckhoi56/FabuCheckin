8dd02646faa699f519e56f9858ba35793a645dd6
Eric Luc <luckhoi89@gmail.com>
2021-10-08 17:05:37 -0700

    ok

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -77,11 +77,10 @@
     if(formSanitizeName()==false){
       console.log('aha')
       m_message_error_modal = true;
-      console.log(m_message_error)
-      console.log(m_message_error_modal)
-      return
+      return false
     }
     
+    console.log('cool')
     const res = await fetch("http://localhost:4242/send-checkin-update", {
       method: "POST",
       headers: {
@@ -89,10 +88,12 @@
       },
       body: JSON.stringify(m_customer_appointment),
     });
-
+    
     const json = await res.json();
-    m_customer_update = false
-    return true
+    m_confirmation_modal=true
+    m_customer_update = m_customer_found= false
+    
+    
   }
   function formSanitizePhone() {
     if(/^(1\s|1|)?((\(\d{3}\))|\d{3})(\-|\s)?(\d{3})(\-|\s)?(\d{4})$/.test(m_customer_appointment["phoneNumbers"]) == false ){
@@ -356,105 +357,7 @@
   </div>
 {/if}
 
-{#if m_confirmation_modal == true}
-  <!-- This example requires Tailwind CSS v2.0+ -->
-  <div
-    transition:fade
-    class="fixed z-10 inset-0 overflow-y-auto"
-    aria-labelledby="modal-title"
-    role="dialog"
-    aria-modal="true"
-  >
-    <div
-      class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0"
-    >
-      <!--
-      Background overlay, show/hide based on modal state.
 
-      Entering: "ease-out duration-300"
-        From: "opacity-0"
-        To: "opacity-100"
-      Leaving: "ease-in duration-200"
-        From: "opacity-100"
-        To: "opacity-0"
-    -->
-      <div
-        class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
-        aria-hidden="true"
-      />
-
-      <!-- This element is to trick the browser into centering the modal contents. -->
-      <span
-        class="hidden sm:inline-block sm:align-middle sm:h-screen"
-        aria-hidden="true">&#8203;</span
-      >
-
-      <!--
-      Modal panel, show/hide based on modal state.
-
-      Entering: "ease-out duration-300"
-        From: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-        To: "opacity-100 translate-y-0 sm:scale-100"
-      Leaving: "ease-in duration-200"
-        From: "opacity-100 translate-y-0 sm:scale-100"
-        To: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-    -->
-
-      <div
-        class="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6"
-      >
-        <div>
-          <div
-            class="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-green-100"
-          >
-            <!-- Heroicon name: outline/check -->
-            <svg
-              class="h-6 w-6 text-green-600"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              aria-hidden="true"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M5 13l4 4L19 7"
-              />
-            </svg>
-          </div>
-
-          <div class="mt-3 text-center sm:mt-5">
-            <h3
-              class="text-lg leading-6 font-medium text-gray-900"
-              id="modal-title"
-            >
-              Checkin successful
-            </h3>
-            <div class="mt-2">
-              <p class="text-sm text-gray-500">
-                A confirmation message will be sent
-              </p>
-            </div>
-          </div>
-        </div>
-        <div class="mt-5 sm:mt-6">
-          <button
-            on:click={() => {
-              m_modal = m_confirmation_modal = false;
-            }}
-            type="button"
-            class="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-yellow-700 text-base font-medium text-white hover:bg-yellow-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm"
-          >
-            Go back to homepage
-          </button>
-          
-        </div>
-      </div>
-    </div>
-  </div>
-{/if}
 
 {#if m_customer_found == true}
   <!-- This example requires Tailwind CSS v2.0+ -->
@@ -830,6 +733,105 @@
           >
             return
           </button>
+        </div>
+      </div>
+    </div>
+  </div>
+{/if}
+{#if m_confirmation_modal == true}
+  <!-- This example requires Tailwind CSS v2.0+ -->
+  <div
+    transition:fade
+    class="fixed z-10 inset-0 overflow-y-auto"
+    aria-labelledby="modal-title"
+    role="dialog"
+    aria-modal="true"
+  >
+    <div
+      class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0"
+    >
+      <!--
+      Background overlay, show/hide based on modal state.
+
+      Entering: "ease-out duration-300"
+        From: "opacity-0"
+        To: "opacity-100"
+      Leaving: "ease-in duration-200"
+        From: "opacity-100"
+        To: "opacity-0"
+    -->
+      <div
+        class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
+        aria-hidden="true"
+      />
+
+      <!-- This element is to trick the browser into centering the modal contents. -->
+      <span
+        class="hidden sm:inline-block sm:align-middle sm:h-screen"
+        aria-hidden="true">&#8203;</span
+      >
+
+      <!--
+      Modal panel, show/hide based on modal state.
+
+      Entering: "ease-out duration-300"
+        From: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+        To: "opacity-100 translate-y-0 sm:scale-100"
+      Leaving: "ease-in duration-200"
+        From: "opacity-100 translate-y-0 sm:scale-100"
+        To: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+    -->
+
+      <div
+        class="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6"
+      >
+        <div>
+          <div
+            class="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-green-100"
+          >
+            <!-- Heroicon name: outline/check -->
+            <svg
+              class="h-6 w-6 text-green-600"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              aria-hidden="true"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M5 13l4 4L19 7"
+              />
+            </svg>
+          </div>
+
+          <div class="mt-3 text-center sm:mt-5">
+            <h3
+              class="text-lg leading-6 font-medium text-gray-900"
+              id="modal-title"
+            >
+              Checkin successful
+            </h3>
+            <div class="mt-2">
+              <p class="text-sm text-gray-500">
+                A confirmation message will be sent
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="mt-5 sm:mt-6">
+          <button
+            on:click={() => {
+              m_modal = m_confirmation_modal = false;
+            }}
+            type="button"
+            class="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-yellow-700 text-base font-medium text-white hover:bg-yellow-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm"
+          >
+            Go back to homepage
+          </button>
+          
         </div>
       </div>
     </div>
